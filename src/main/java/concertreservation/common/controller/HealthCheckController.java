ed1505de2487af8215f6a242066f382af092279d
qheog <qheogus55@naver.com>
2025-02-09 10:22:44 +0900

    [#28]feat: HealthCheck를 위한 컨트롤러 생성

---
+++ src/main/java/concertreservation/common/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package concertreservation.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Success Health Check";
+    }
+}
